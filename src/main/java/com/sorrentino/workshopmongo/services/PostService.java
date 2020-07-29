c79164b536e7ed74cfd6910cacf1f327b52e238d
FSorrentinoC <fabiano.sorrentino@gmail.com>
2020-07-29 18:06:26 -0300

    Obtendo um post por id

---
+++ src/main/java/com/sorrentino/workshopmongo/services/PostService.java
@@ -0,0 +1,57 @@
+package com.sorrentino.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sorrentino.workshopmongo.domain.User;
+import com.sorrentino.workshopmongo.dto.UserDTO;
+import com.sorrentino.workshopmongo.repository.UserRepository;
+import com.sorrentino.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private UserRepository repo;
+
+	public List<User> findAll() {
+		return repo.findAll();
+	}
+
+	public User findById(String id) {
+		User user = repo.findById(id);
+		if (user == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}
+		return user;
+	}
+
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+
+	public void delete(String id) {
+		findById(id);
+		repo.deleteAll(id);
+
+	}
+
+	public User update(User obj) {
+		User newObj = repo.findById(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
+
+}
