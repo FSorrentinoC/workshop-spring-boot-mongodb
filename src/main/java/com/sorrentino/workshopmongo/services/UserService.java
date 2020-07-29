44a75d51deba3fcab32dfee34a6785f3594a1dc9
FSorrentinoC <fabiano.sorrentino@gmail.com>
2020-07-29 15:54:36 -0300

    Inserção de usuário com POST

--- src/main/java/com/sorrentino/workshopmongo/services/UserService.java
+++ src/main/java/com/sorrentino/workshopmongo/services/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.sorrentino.workshopmongo.domain.User;
+import com.sorrentino.workshopmongo.dto.UserDTO;
 import com.sorrentino.workshopmongo.repository.UserRepository;
 import com.sorrentino.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -25,5 +26,11 @@ public class UserService {
 			throw new ObjectNotFoundException("Objeto não encontrado");
 		}
 		return user;
+	}
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
