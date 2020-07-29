9c9d64db9760748081002b8db74ddb9d2a8932f7
FSorrentinoC <fabiano.sorrentino@gmail.com>
2020-07-29 12:17:38 -0300

    Conectando ao MongoDB com repository e servicen noOk

---
+++ src/main/java/com/sorrentino/workshopmongo/services/UserService.java
@@ -0,0 +1,20 @@
+package com.sorrentino.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sorrentino.workshopmongo.domain.User;
+import com.sorrentino.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll(){
+		return repo.findAll();
+	}
+}
