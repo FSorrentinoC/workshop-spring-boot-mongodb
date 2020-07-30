0bf40042320f419fe6516440ee765767b1d73a44
FSorrentinoC <fabiano.sorrentino@gmail.com>
2020-07-30 11:25:33 -0300

    Reajustados o PostService e o UserService

--- src/main/java/com/sorrentino/workshopmongo/services/PostService.java
+++ src/main/java/com/sorrentino/workshopmongo/services/PostService.java
@@ -2,6 +2,7 @@ package com.sorrentino.workshopmongo.services;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,20 +18,16 @@ public class PostService {
 	private PostRepository repo;
 
 	public Post findById(String id) {
-		Post user = repo.findOne(id);
-		if (user == null) {
-			throw new ObjectNotFoundException("Objeto não encontrado");
-		}
-		return user;
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
 	public List<Post> findByTitle(String text) {
 		return repo.searchTitle(text);
 	}
-	
-	public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
 		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
 		return repo.fullSearch(text, minDate, maxDate);
 	}
-
 }
--- src/main/java/com/sorrentino/workshopmongo/services/UserService.java
+++ src/main/java/com/sorrentino/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.sorrentino.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,11 +22,8 @@ public class UserService {
 	}
 
 	public User findById(String id) {
-		User user = repo.findById(id);
-		if (user == null) {
-			throw new ObjectNotFoundException("Objeto não encontrado");
-		}
-		return user;
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
 	public User insert(User obj) {
@@ -34,12 +32,11 @@ public class UserService {
 
 	public void delete(String id) {
 		findById(id);
-		repo.deleteAll(id);
-
+		repo.deleteById(id);
 	}
 
 	public User update(User obj) {
-		User newObj = repo.findById(obj.getId());
+		User newObj = findById(obj.getId());
 		updateData(newObj, obj);
 		return repo.save(newObj);
 	}
@@ -47,11 +44,9 @@ public class UserService {
 	private void updateData(User newObj, User obj) {
 		newObj.setName(obj.getName());
 		newObj.setEmail(obj.getEmail());
-
 	}
 
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
-
 }
