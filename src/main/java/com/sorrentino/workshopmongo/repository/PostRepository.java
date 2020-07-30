92c0b25ab87ff7485f98aadc1619de4c804bd23d
FSorrentinoC <fabiano.sorrentino@gmail.com>
2020-07-30 10:07:54 -0300

    Consulta simples com query methods

--- src/main/java/com/sorrentino/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/sorrentino/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,8 @@
 package com.sorrentino.workshopmongo.repository;
 
+import java.util.List;
+
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +10,5 @@ import com.sorrentino.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
-	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
--- src/main/java/com/sorrentino/workshopmongo/services/PostService.java
+++ src/main/java/com/sorrentino/workshopmongo/services/PostService.java
@@ -5,8 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.sorrentino.workshopmongo.domain.User;
-import com.sorrentino.workshopmongo.dto.UserDTO;
+import com.sorrentino.workshopmongo.domain.Post;
 import com.sorrentino.workshopmongo.repository.UserRepository;
 import com.sorrentino.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -16,42 +15,16 @@ public class PostService {
 	@Autowired
 	private UserRepository repo;
 
-	public List<User> findAll() {
-		return repo.findAll();
-	}
-
-	public User findById(String id) {
-		User user = repo.findById(id);
+	public Post findById(String id) {
+		Post user = repo.findOne(id);
 		if (user == null) {
 			throw new ObjectNotFoundException("Objeto não encontrado");
 		}
 		return user;
 	}
-
-	public User insert(User obj) {
-		return repo.insert(obj);
+	
+	public List<Post> findByTitle(String text){
+		return repo.findByTitleContainingIgnoreCase(text);
 	}
-
-	public void delete(String id) {
-		findById(id);
-		repo.deleteAll(id);
-
-	}
-
-	public User update(User obj) {
-		User newObj = repo.findById(obj.getId());
-		updateData(newObj, obj);
-		return repo.save(newObj);
-	}
-
-	private void updateData(User newObj, User obj) {
-		newObj.setName(obj.getName());
-		newObj.setEmail(obj.getEmail());
-
-	}
-
-	public User fromDTO(UserDTO objDto) {
-		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
-	}
-
+	
 }
